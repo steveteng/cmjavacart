3ed8c07d83f38cc4e384f643895ace32e8ea1c60
Steve Teng <steveteng@nbnco.com.au>
2014-07-30 13:29:07 +1000

    s

--- usr/template/diy/testrubyserver.rb
+++ usr/template/diy/testrubyserver.rb
@@ -2,17 +2,17 @@
 require 'webrick'
 include WEBrick
 
-config = {}
-config.update(:Port => 8080)
-config.update(:BindAddress => ARGV[0])
-config.update(:DocumentRoot => ARGV[1])
-server = HTTPServer.new(config)
-['INT', 'TERM'].each {|signal|
-  trap(signal) {server.shutdown}
-}
+#config = {}
+#config.update(:Port => 8080)
+#config.update(:BindAddress => ARGV[0])
+#config.update(:DocumentRoot => ARGV[1])
+#server = HTTPServer.new(config)
+#['INT', 'TERM'].each {|signal|
+#  trap(signal) {server.shutdown}
+#}
 
-server.mount_proc '/' do |req, res|
-  res.body = File.read 'index1.html'
-end
+#server.mount_proc '/' do |req, res|
+#  res.body = File.read 'index1.html'
+#end
 
-server.start
+#server.start
